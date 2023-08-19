fba2f30222df7062b1d99eebe44f48d27e26aa01
Sharvari-d9 <sharvari.dongre9@gmail.com>
2023-08-20 03:41:30 +0530

    added new modules

---
+++ Pluralsight Course notes/Module 03/classes.js
@@ -0,0 +1,30 @@
+//Supported features for class
+/**
+ * Constructors
+ * Static initialization blocks
+ * public metthods and fields
+ * statc methods and fields
+ * private methods and fieldsInheritances
+ * 
+ */
+
+class CalendarDay{
+    #month;
+    #day;
+    #year;
+    //constructor
+    constructor (month,day,year){
+        this.month = month;
+        this.day = day;
+        this.year = year
+    }
+    //Public method(
+
+    toString()
+    {
+        return lnsear${this.month+1}
+    }
+}
+
+let day1 =new CalendarDay(2023,0,1);
+console.log(day1.totring());
