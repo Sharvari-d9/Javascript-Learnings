6550f56d853a160b9134df4fc0ff4b5061bdae00
Sharvari-d9 <sharvari.dongre9@gmail.com>
2023-08-26 02:09:20 +0530

    updated notes

--- Pluralsight Course notes/Module 07/index.js
+++ Pluralsight Course notes/Module 07/index.js
@@ -2,14 +2,28 @@ import employess from'./data.json' assert{type: 'json'}
 import createPrompt from 'prompt-sync'
 let prompt = createPrompt();
 
+const logEmployee = (employee) => {
+    Object.entries(employee).forEach(entry =>{
+        console.log(`${entry[0]} : ${entry[1]}`)
+    });
+}
+
 function getInput(promptText,validator, transformer){
     let value = prompt(promptText);
     if(validator && !validator(value)){
         console.error(`--Inalid input`);
         process.exit(1);
     }
+    if(transformer){
+        return transformer(value);
+    }
     return value;
 }
+
+//transformer function
+// const trasformBoolean = (input) =>{
+//     return(input === "yes");
+// }
 
 //Validator functions
 const isStringInputValid = function(input){
@@ -43,6 +57,38 @@ const isStartDayValid = function(input){
     }
     return true;
 }
+// execution Functions
+function searchByName(){
+    const emp_name = getInput("Enter First name: ").toLowerCase();
+    const lastNameSearch = getInput("Enter Last Name: ").toLowerCase();
+    const result = employess.filter(e=> { //used to filter the array
+
+        if(emp_name && !e.firstName.toLowerCase().includes(emp_name)){
+            return false
+        };
+        if(lastNameSearch && !e.lastName.toLowerCase().includes(lastNameSearch)){
+            return false;
+        };
+        return(true);
+    });
+        result.forEach((e,idx) => {
+            console.log("");
+            console.log(`Search Result ${idx +1} -------------------------`);
+            logEmployee(e);
+        });
+
+  }
+
+function searchById(){  
+    const id = getInput("Employee Id: ", null, Number);
+    const result = employess.find(e=> e.id ===id); //using find function for arrays
+    if(result){
+        console.log("");
+        logEmployee(result);
+    }else{
+        console.log("No results...");
+    }
+}
 
 function addEmployees(){
     let employee = {};
@@ -50,18 +96,27 @@ function addEmployees(){
    console.log(`Add Employee ------------------`);
    console.log('');
    
-   employee.firstName = getInput("First Name: ", isStringInputValid);
+   employee.firstName = getInput("First Name: ", isStringInputValid); //passing the prompt and whole function (not calling the function)
    employee.lastName  = getInput("Last Name: ", isStringInputValid);
    let startDateYear = getInput("Emplpoyee Start Year(1990-2023: ",isStartYearValid);
    let startDateMonth = getInput("Employee sart month Year: ",isStartMonthValid);
    let startDateDay = getInput("Enployee start Date Day(1-31",isStartDayValid);
    employee.startDate = new Date(startDateYear,startDateMonth,startDateDay);
-   employee.isActive = getInput("Is employee active (yes or no): ",isBooleanInputValid); 
+   employee.isActive = getInput("Is employee active (yes or no): ",isBooleanInputValid,i => (i==="yes")); //inline arrow function (compact way to have function), we dont even need () function paranthesis
     // Output Employee JSON
     const json = JSON.stringify(employee, null, 2);
     console.log(`Employee: ${json}`);
 }
 
+function listEmployees(){
+    console.log('Employee List ----------------------');
+    console.log('');
+    employess.forEach(e=>{
+        logEmployee(e);
+        prompt('Press enter to continue....');
+    });
+    console.log(`Employee list completed`);
+}
 // Applicatin execution 
 
 const command = process.argv[2].toLowerCase();
@@ -72,6 +127,14 @@ switch(command){
 
     case 'add':
         addEmployees();
+        break;
+
+    case 'search-by-id':
+        searchById();
+        break;
+    
+    case 'search-by-name':
+        searchByName();
         break;
         
     default:
