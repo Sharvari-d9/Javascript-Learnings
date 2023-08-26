fa6dcd05e93ed141953559f81671a3405c90ecb3
Sharvari-d9 <sharvari.dongre9@gmail.com>
2023-08-27 00:26:27 +0530

    updated with topics

---
+++ Pluralsight Course notes/Module 08/async-await.js
@@ -0,0 +1,21 @@
+//Import Node FilseSystem Module (the promises version)
+import fs from "node:fs/promises";
+
+async function loadData(){
+    try{
+        const data = await fs.readFile('./data.json', 'utf-8');
+        await fs.readFile('./data.json','utf8')
+        console.log("File Read 1")
+        await fs.readFile('./data.json','utf8')
+        console.log("File Read 2")
+        await fs.readFile('./data.json','utf8')
+        console.log("File Read 3")
+        const dataObj = JSON.parse(data);
+        console.log(dataObj);
+        console.log("Complete");
+    }catch(err){
+        console.log("Could not load and parse file ")
+        throw(err);
+    }
+}
+loadData().then(()=> console.log("Promise compeleted"));
